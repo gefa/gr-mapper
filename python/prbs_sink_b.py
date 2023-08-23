5605499287aca0f2e5bccefd1121f405d6bcd7a1
stefan gvozdenovic <tesla@bu.edu>
2023-08-23 19:52:11 +0000

    remove python grand implementation from PRBS sink block, instead we use C++ GRAND in CRC block, add trx with hier tx & rx blocks

--- python/prbs_sink_b.py
+++ python/prbs_sink_b.py
@@ -6,7 +6,7 @@ from gnuradio import gr
 import prbs_base
 import resource
 import time
-
+'''
 def permuteUnique(nums):
     res =[]
     perm = []
@@ -26,9 +26,9 @@ def permuteUnique(nums):
                 perm.pop()
     dfs()
     return np.array(res)
-
+'''
 class prbs_sink_b(gr.sync_block):
-    def __init__(self, which_mode="PRBS31", reset_len=100000,grand=0, skip=100000):
+    def __init__(self, which_mode="PRBS31", reset_len=100000, skip=100000):
         gr.sync_block.__init__(self,
             name="prbs_sink_b",
             in_sig=[numpy.int8],
@@ -36,12 +36,13 @@ class prbs_sink_b(gr.sync_block):
         self.base = prbs_base.prbs_base(which_mode, reset_len)
         self.nbits = 0.0
         self.nerrs = 0.0
-        self.grand = grand
+        self.grand = 0 #grand
         self.skip  = skip
         self.work_time = 0
         self.mem = 0
         self.n = reset_len
         #self.f0 = permuteUnique( (self.n)*[0] ) # no correction
+        '''
         if self.grand==1:
           self.f1 = permuteUnique( (self.n-1)*[0]+[1] )
           self.flips = np.concatenate((self.f1)).astype('int8')
@@ -54,6 +55,7 @@ class prbs_sink_b(gr.sync_block):
           self.f2 = permuteUnique( (self.n-2)*[0]+[1,1] )
           self.f3 = permuteUnique( (self.n-3)*[0]+[1,1,1] )
           self.flips = np.concatenate((self.f1, self.f2, self.f3)).astype('int8')
+        '''
     def work(self, input_items, output_items):
         #t1_start = time.time()
         inb = input_items[0]
@@ -61,18 +63,19 @@ class prbs_sink_b(gr.sync_block):
         gen = self.base.gen_n(len(inb))
         if(self.nitems_read(0) > self.skip):
             # only count bit errors after first skip bits
-            
+            '''
             step=self.n
             grand = [0,0,0,0]
-            for i, j in zip(np.arange(0, len(inb), step), np.arange(0, len(gen), step)):
+            if self.grand>0:
+             for i, j in zip(np.arange(0, len(inb), step), np.arange(0, len(gen), step)):
               subinb = inb[i:i+step]
               subgen = gen[j:j+step]
-              '''
-              nerr= numpy.sum(numpy.bitwise_xor(subinb, subgen).astype('float32'))
-              bitd = int(nerr)
-              if (bitd>=0 and bitd <=3):
-                grand[bitd] = grand[bitd] + 1
-              '''
+              
+              #nerr= numpy.sum(numpy.bitwise_xor(subinb, subgen).astype('float32'))
+              #bitd = int(nerr)
+              #if (bitd>=0 and bitd <=3):
+              #  grand[bitd] = grand[bitd] + 1
+              
               if self.grand>0:
                 # grand correct bits
                 isfixed=False
@@ -88,7 +91,8 @@ class prbs_sink_b(gr.sync_block):
             
 #            print("GRAND ",grand)
             if self.grand==0:
-              self.nerrs += numpy.sum(numpy.bitwise_xor(inb, gen).astype('float32'))
+            '''
+            self.nerrs += numpy.sum(numpy.bitwise_xor(inb, gen).astype('float32'))
             self.nbits += len(inb)
         if self.nbits > 0:
             print "NBits: %d \tNErrs: %d \tBER: %.4E"%(int(self.nbits), int(self.nerrs), self.nerrs/self.nbits) #, self.work_time, print_memory_usage()) #\tTime: %.4E \tMem: %d
